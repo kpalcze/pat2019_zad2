89101952050f5a453bfd2ca9175048c3f794bcf4
Krzysztof Palczewski <kpalcze@gmail.com>
2019-01-09 01:38:17 +0100

    ADD: Menu with links to raw data

--- pat2019/mainapp/views.py
+++ pat2019/mainapp/views.py
@@ -8,8 +8,22 @@ from django.template import loader
 from .models import Salary
 
 def index(request):
-    salary_list = Salary.objects.all()
     template = loader.get_template('mainapp/index.html')
+    return HttpResponse(template.render({}, request))
+
+
+def raw_data(request):
+    salary_list = Salary.objects.all()
+    template = loader.get_template('mainapp/data_table.html')
+    context = {
+        'salary_list': salary_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def predicted_data(request):
+    salary_list = Salary.objects.all()
+    template = loader.get_template('mainapp/data_table.html')
     context = {
         'salary_list': salary_list,
     }
