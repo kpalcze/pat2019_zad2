18ee8dce9d935b6ca7348faa57280e6ccde06ebd
Krzysztof Palczewski <kpalcze@gmail.com>
2019-01-09 15:51:22 +0100

    ADD: Add to project module with implemented linear regression

---
+++ pat2019/mainapp/machinelearning/linearregression.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+
+def prepare_data(salary):
+    salary['salaryBrutto'] = pd.to_numeric(salary['salaryBrutto'], errors='coerce')
+    salary_train = salary[salary['salaryBrutto'].notnull()]
+    salary_test = salary[salary['salaryBrutto'].isnull()]
+    return salary_train, salary_test
+
+
+def train_model(salary_train):
+    X = salary_train[['workedYears']]
+    y = salary_train['salaryBrutto']
+    model = LinearRegression()
+    model.fit(X,y)
+    return model
+
+
+def predict_salary(model, salary_test):
+    predictions = model.predict(salary_test[['workedYears']])
+    salary_test.loc[:, 'salaryBruttoPredicted'] = pd.Series(predictions, index=salary_test.index)
+    return salary_test
+
+
+def save_results_to_db(salary_train, salary_test):
+    salary_out = salary_train.append(salary_test)
+    salary_out = salary_out[['id', 'workedYears', 'salaryBrutto', 'salaryBruttoPredicted']]
+    # TODO insert to db
+
+
+def calculate_predicted_salary(data):
+    salary = pd.DataFrame(list(data.values()))
+    salary_train, salary_test = prepare_data(salary)
+    model = train_model(salary_train)
+    salary_test = predict_salary(model, salary_test)
+    save_results_to_db(salary_train, salary_test)
+
+
+
